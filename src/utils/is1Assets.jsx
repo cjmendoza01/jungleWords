3cb4517ca82afdfeae0c910c482acce7cd95e640
julsmendozaaa <julsmendozaaa@gmail.com>
2024-09-03 16:58:59 +0800

    juls

---
+++ src/utils/is1Assets.jsx
@@ -0,0 +1,12 @@
+
+// Import all images/assets for intermediate stage 1
+import egg1 from "../assets/is1/egg1.png";
+import egg2 from "../assets/is1/egg2.png";
+import egg3 from "../assets/is1/egg3.png";
+import egg4 from "../assets/is1/egg4.png";
+import egg5 from "../assets/is1/egg5.png";
+import egg6 from "../assets/is1/egg6.png";
+import egg7 from "../assets/is1/egg7.png";
+import egg8 from "../assets/is1/egg8.png";
+
+// wala pa po yung mga animations hehe
