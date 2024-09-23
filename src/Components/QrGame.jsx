72c72f27e9bb6c20e69b7e11769b00913fbb7d45
julsmendozaaa <julsmendozaaa@gmail.com>
2024-09-24 01:20:42 +0800

    advstage1 - change bg and move the cam n border (di lang mausog pa left konti hehe)

--- src/Components/QrGame.jsx
+++ src/Components/QrGame.jsx
@@ -148,6 +148,26 @@ export default function QRGame() {
 					width: "50%",
 				}}
 			>
+			
+			{/* backgroun animation */}
+			<video
+				autoPlay
+				muted
+				loop
+				style={{
+					position: "fixed",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%",
+					objectFit: "cover",
+					zIndex: "1",
+				}}
+			>
+				<source src="/bgs1.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+
 				<div
 					style={{
 						display: "flex",
