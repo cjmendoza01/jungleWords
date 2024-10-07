3eb033370761618ed837e6cfc74b098402eaeeb4
cjmendoza01 <cyrill_mendoza@yahoo.com>
2024-10-07 10:30:17 +0800

    btn

--- src/Components/Modals/DisplayModal.jsx
+++ src/Components/Modals/DisplayModal.jsx
@@ -37,7 +37,7 @@ export default function DisplayModal({ item, closeModal }) {
 				</div>
 				<div className="modalCloseBtnDiv">
 					<div className="modalCloseBtn" onClick={() => closeModal()}>
-						X
+						{/* X */}
 					</div>
 				</div>
 
