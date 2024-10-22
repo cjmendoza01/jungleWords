b470b1d5be97bfa51cddcf92b8366a41a30f6456
cjmendoza01 <cyrill_mendoza@yahoo.com>
2024-10-22 10:23:07 +0800

    policy

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,13 @@ import { Helmet } from "react-helmet";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Helmet>
-				<meta
+			{/* <Helmet> */}
+			{/* <meta
 					http-equiv="Content-Security-Policy"
 					content="default-src 'self'; img-src 'self' https://junglewords.org; script-src 'self' https://junglewords.org; style-src 'self' 'unsafe-inline';"
 				/>
-				<meta http-equiv="X-Frame-Options" content="SAMEORIGIN" />
-			</Helmet>
+				<meta http-equiv="X-Frame-Options" content="SAMEORIGIN" /> */}
+			{/* </Helmet> */}
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
