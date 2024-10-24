1e6059a8c04453398e90082a116efc870f6a4817
cjmendoza01 <cyrill_mendoza@yahoo.com>
2024-10-24 14:17:35 +0800

    updates

--- src/Components/SpecialEducation.jsx
+++ src/Components/SpecialEducation.jsx
@@ -2,117 +2,147 @@ import React from "react";
 import "./SPED.css";
 
 function SpecialEducation() {
-  return (
-    <div id="specEd">
-      <div className="App">
-        <header>
-          <audio autoPlay loop src={"/BgMusic.mp3"} />
-          <nav className="nav">
-            <div className="logo">
-              <img src="/Logo.png" alt="JW Logo" />
-            </div>
-            <div className="nav-linksz">
-              <a href="/" className="home-linkz">
-                <img src="/home.png" alt="Home" />
-              </a>
-            </div>
-          </nav>
-        </header>
-        <main>
-          <div className="jungle-background">
-            <div className="content">
-              <img src="/specEd.png" alt="Special Education Image" />
+	return (
+		<div id="specEd">
+			<div className="App">
+				<header>
+					<audio autoPlay loop src={"/BgMusic.mp3"} />
+					<nav className="nav">
+						<div className="logo">
+							<img src="/Logo.png" alt="JW Logo" />
+						</div>
+						<div className="nav-linksz">
+							<a href="/" className="home-linkz">
+								<img src="/home.png" alt="Home" />
+							</a>
+						</div>
+					</nav>
+				</header>
+				<main>
+					<div className="jungle-background-sped">
+						<div className="contents_spec_ed">
+							<div
+								style={{
+									width: "100%",
+									display: "flex",
+									justifyContent: "center",
+								}}
+							>
+								<img src="/specEd.png" alt="Special Education Image" />
+							</div>
+							<div className="text-content">
+								<h2>
+									{" "}
+									Government Support and Policies Supporting SPED in the
+									Philippines
+								</h2>
+								<p>
+									Every child deserves equal opportunities, regardless of the
+									challenges they encounter. Special education plays a crucial
+									role for children with learning disabilities, as it provides
+									them with access to quality education tailored to their
+									specific needs. This support helps students achieve
+									independence and reach their full potential. For some, clear
+									and effective speech can be a difficult and intricate form of
+									communication. Learning disabilities cover a broad spectrum of
+									challenges, including speech and language disorders, as well
+									as motor skill difficulties.
+								</p>
 
-              <div className="text-content"> 
-			      	<h2> Government Support and Policies Supporting SPED in the Philippines</h2>
-                <p>
-                  Every child deserves equal opportunities, regardless of the
-                  challenges they encounter. Special education plays a crucial
-                  role for children with learning disabilities, as it provides
-                  them with access to quality education tailored to their
-                  specific needs. This support helps students achieve
-                  independence and reach their full potential. For some, clear
-                  and effective speech can be a difficult and intricate form of
-                  communication. Learning disabilities cover a broad spectrum of
-                  challenges, including speech and language disorders, as well
-                  as motor skill difficulties.
-                </p>
+								<p>
+									The Department of Education in the Philippines highlights the
+									Special Education (SPED) program as an important facet of the
+									country’s educational system as it aims to enhance access to
+									learning materials and to increase the efficiency of special
+									education services for learners with learning disabilities in
+									elementary and secondary schools. The current efforts in the
+									field of Speech and Language Development (SLD) initiatives for
+									Filipino children reveal a predominant emphasis on improving
+									education, preventive measures, regulation, and ensuring equal
+									rights for all learners. A recent study by the United Nations
+									Children’s Fund (UNICEF, 2022) estimates that around 1.6
+									million Filipino children have disabilities. According to data
+									from the Department of Education (DepEd) for the 2016-2017
+									school year, 232,975 students with disabilities or learning
+									exceptionalities were integrated into regular classes.
+								</p>
+								<br></br>
+								<h2>Learning Exceptionalities</h2>
+								<p>
+									The Department of Education (DepEd) categorizes
+									exceptionalities into two groups:
+									<p>
+										a. those with a diagnosis from a specialist (11 conditions)
+										and{" "}
+									</p>
+									b. those based on manifestation (five conditions). Among the
+									diagnosed conditions, intellectual disability was the most
+									common among elementary students, with 20,377 cases. Regarding
+									manifestation, high prevalence rates were observed for
+									difficulties with vision, as well as challenges in
+									remembering, concentrating, paying attention, and
+									understanding, affecting both elementary and secondary
+									learners, with totals of 41,317 and 64,338, respectively.
+								</p>
+								<br></br>
+								<h2>SPED Teachers</h2>
+								<p>
+									DepEd teachers who underwent formal training in SPED at the
+									national, regional, and division level and who are presently
+									teaching children with special needs are considered as SPED
+									teachers. These educators possess specialized skills and
+									knowledge tailored to address the unique learning challenges
+									of children with disabilities. Their continuous development
+									and dedication ensure that inclusive education practices are
+									effectively implemented, fostering an environment where all
+									learners can thrive.
+								</p>
 
-                <p>
-                  The Department of Education in the Philippines highlights the
-                  Special Education (SPED) program as an important facet of the
-                  country’s educational system as it aims to enhance access to
-                  learning materials and to increase the efficiency of special
-                  education services for learners with learning disabilities in
-                  elementary and secondary schools. The current efforts in the
-                  field of Speech and Language Development (SLD) initiatives for
-                  Filipino children reveal a predominant emphasis on improving
-                  education, preventive measures, regulation, and ensuring equal
-                  rights for all learners. A recent study by the United Nations
-                  Children’s Fund (UNICEF, 2022) estimates that around 1.6
-                  million Filipino children have disabilities. According to data
-                  from the Department of Education (DepEd) for the 2016-2017
-                  school year, 232,975 students with disabilities or learning
-                  exceptionalities were integrated into regular classes.
-                </p>
-                      <br></br>
-                <h2>Learning Exceptionalities</h2>
-                <p>
-                  The Department of Education (DepEd) categorizes
-                  exceptionalities into two groups:
-                  <p>a. those with a diagnosis from a specialist (11 conditions) and </p>
-                  b. those based on manifestation (five conditions). Among the
-                  diagnosed conditions, intellectual disability was the most
-                  common among elementary students, with 20,377 cases.
-                  Regarding manifestation, high prevalence rates were observed
-                  for difficulties with vision, as well as challenges in
-                  remembering, concentrating, paying attention, and
-                  understanding, affecting both elementary and secondary
-                  learners, with totals of 41,317 and 64,338, respectively.
-                </p>
-                  <br></br>
-                <h2>SPED Teachers</h2>
-                <p>
-                  DepEd teachers who underwent formal training in SPED at the
-                  national, regional, and division level and who are presently
-                  teaching children with special needs are considered as SPED
-                  teachers. These educators possess specialized skills and
-                  knowledge tailored to address the unique learning challenges
-                  of children with disabilities. Their continuous development
-                  and dedication ensure that inclusive education practices are
-                  effectively implemented, fostering an environment where all
-                  learners can thrive.
-                </p>
+								<br></br>
+								<h2>SPED Learners by Region</h2>
+								<p>
+									Region IV-A had the highest number of learners with
+									exceptionalities integrated into regular classes, totaling
+									34,644 or 14.9%. This was closely followed by the National
+									Capital Region (NCR), with 33,977 learners or 14.6%. Region XI
+									had 17,601 learners (7.6%), and Region III had 17,295 (7.4%).
+									In contrast, the BARMM and CARAGA regions reported the lowest
+									numbers, with 2,568 and 4,227 learners, accounting for 1.1%
+									and 1.8%, respectively.
+								</p>
 
-                  <br></br>
-                <h2>SPED Learners by Region</h2>
-                <p>
-                  Region IV-A had the highest number of learners with
-                  exceptionalities integrated into regular classes, totaling
-                  34,644 or 14.9%. This was closely followed by the National
-                  Capital Region (NCR), with 33,977 learners or 14.6%. Region XI
-                  had 17,601 learners (7.6%), and Region III had 17,295 (7.4%).
-                  In contrast, the BARMM and CARAGA regions reported the lowest
-                  numbers, with 2,568 and 4,227 learners, accounting for 1.1%
-                  and 1.8%, respectively.
-                </p>
-
-                <div className="reference-section">
-					<br></br>
-					<br></br>
-					<br></br>
-                  <h3>References</h3>
-                  <p>[1] "Special Education (SPED) Profile in the Philippines," Congressional Policy and Budget Research Department. [Online]. Available: https://cpbrd.congress.gov.ph/2012-06-30-13-06-51/2012-06-30-13-36-50/1604-ff2023-17-special-education-sped-profile-in-the-philippines. [Accessed: Oct. 23, 2024].</p>
-                  <p>[2] A. B. Author, "Title of the Document," ScholarWorks @ UNI, [Online]. Available: https://scholarworks.uni.edu/cgi/viewcontent.cgi?article=1909&context=grp. [Accessed: Oct. 23, 2024].</p>
-				  <p>[3] Author(s), "Title of the Article," KnE Social Sciences, [Online]. Available: https://kneopen.com/KnE-Social/article/view/10592/. [Accessed: Oct. 23, 2024].</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
+								<div className="reference-section">
+									<br></br>
+									<br></br>
+									<br></br>
+									<h3>References</h3>
+									<p>
+										[1] "Special Education (SPED) Profile in the Philippines,"
+										Congressional Policy and Budget Research Department.
+										[Online]. Available:
+										https://cpbrd.congress.gov.ph/2012-06-30-13-06-51/2012-06-30-13-36-50/1604-ff2023-17-special-education-sped-profile-in-the-philippines.
+										[Accessed: Oct. 23, 2024].
+									</p>
+									<p>
+										[2] A. B. Author, "Title of the Document," ScholarWorks @
+										UNI, [Online]. Available:
+										https://scholarworks.uni.edu/cgi/viewcontent.cgi?article=1909&context=grp.
+										[Accessed: Oct. 23, 2024].
+									</p>
+									<p>
+										[3] Author(s), "Title of the Article," KnE Social Sciences,
+										[Online]. Available:
+										https://kneopen.com/KnE-Social/article/view/10592/.
+										[Accessed: Oct. 23, 2024].
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default SpecialEducation;
--- src/Components/SupportSDES.jsx
+++ src/Components/SupportSDES.jsx
@@ -4,104 +4,169 @@ import "./SPED.css";
 function SupportSDES() {
 	return (
 		<div id="specEd">
-		<div className="App">
-		  <header>
-			<audio autoPlay loop src={"/BgMusic.mp3"} />
-			<nav className="nav">
-			  <div className="logo">
-				<img src="/Logo.png" alt="JW Logo" />
-			  </div>
-			  <div className="nav-linksz">
-				<a href="/" className="home-linkz">
-				  <img src="/home.png" alt="Home" />
-				</a>
-			  </div>
-			</nav>
-		  </header>
-		  <main>
-			<div className="jungle-background">
-			<div className="content">
-				<img src="/spedSup.png" alt="Your Image Alt Text" />
-				<div className="text-content">
+			<div className="App">
+				<header>
+					<audio autoPlay loop src={"/BgMusic.mp3"} />
+					<nav className="nav">
+						<div className="logo">
+							<img src="/Logo.png" alt="JW Logo" />
+						</div>
+						<div className="nav-linksz">
+							<a href="/" className="home-linkz">
+								<img src="/home.png" alt="Home" />
+							</a>
+						</div>
+					</nav>
+				</header>
+				<main>
+					<div className="jungle-background-sped">
+						<div className="contents_spec_ed">
+							<div
+								style={{
+									width: "100%",
+									display: "flex",
+									justifyContent: "center",
+								}}
+							>
+								<img src="/spedSup.png" alt="Your Image Alt Text" />
+							</div>
+							<div className="text-content">
+								<h2>
+									{" "}
+									Government Support and Policies Supporting SPED in the
+									Philippines
+								</h2>
+								<p>
+									The development of special education in the Philippines has
+									received considerable support over the years from the
+									government, non-government organizations, and stakeholders,
+									addressing the needs and challenges of the times. Awareness
+									among both government and private sectors regarding the
+									importance of providing equal opportunities for children with
+									special needs has significantly improved.
+								</p>
+								<br></br>
+								<h2> RA 10533 – The Enhanced Basic Education Act </h2>
+								<p>
+									Pursuant to Section 16 of Republic Act No. 10533, entitled "An
+									Act Enhancing the Philippine Basic Education System by
+									Strengthening Its Curriculum and Increasing the Number of
+									Years for Basic Education, Appropriating Funds Therefor and
+									for Other Purposes," otherwise known as the "Enhanced Basic
+									Education Act of 2013," approved on May 15, 2013, and which
+									took effect on June 8, 2013, the Department of Education
+									(DepEd), the Commission on Higher Education (CHED), and the
+									Technical Education and Skills Development Authority (TESDA),
+									hereby issue the following rules and regulations to implement
+									the provisions of the Act.
+								</p>
+								<p>
+									Republic Act No. 10533, also known as the "Enhanced Basic
+									Education Act of 2013," introduced significant reforms to the
+									Philippine education system. The Act aimed to strengthen the
+									curriculum by aligning it with international standards and
+									enhancing students' skillsets. It also increased the number of
+									years in basic education through the implementation of the
+									K-12 program, which includes Kindergarten, six years of
+									elementary education, four years of junior high school, and
+									two additional years of senior high school. Additionally, the
+									Act ensured the allocation of funds to support improvements in
+									infrastructure, teaching materials, and other resources needed
+									to implement these reforms. The Department of Education
+									(DepEd), the Commission on Higher Education (CHED), and the
+									Technical Education and Skills Development Authority (TESDA)
+									were tasked with executing the provisions of the law, focusing
+									on different educational levels: DepEd for basic education,
+									CHED for higher education, and TESDA for technical-vocational
+									education. Ultimately, the law seeks to improve the overall
+									quality of education in the Philippines, equipping students
+									with the necessary skills for higher education and the
+									workforce, and fostering global competitiveness.
+								</p>
+								<br></br>
+								<h2> RA 7277 – The Magna Carta for Disabled Persons</h2>
+								<p>
+									One positive development in special education is the
+									implementation of Republic Act 7277, otherwise known as the
+									Magna Carta for Disabled Persons, an Act providing for the
+									rehabilitiation, self-development and self-reliance of
+									disabled persons and their integration into the mainstream of
+									society. Centers on employment, education, health care, social
+									services, telecommunications, buildings and transportation,
+									and political and civil rights. This Act also prohibits
+									discrimination against disabled persons in employment,
+									transportation, and in public accommodation and services. In
+									support of this legislation, the Department of Education has
+									directed all school divisions in the country to establish
+									Special Education Centers to help provide effective delivery
+									of special education services nationwide.
+								</p>
+								<br></br>
+								<h2>
+									{" "}
+									DepEd Order No. 72, s. 2009 – Guidelines for SPED Programs
+								</h2>
+								<p>
+									The Department of Education (DepEd) in the Philippines issued
+									DepEd Order No. 72, s. 2009 to promote inclusive education as
+									a strategy to increase school participation among children,
+									especially those with disabilities or special needs. The order
+									encourages schools to adopt child-friendly environments and
+									ensure that children with diverse learning needs are given the
+									opportunity to learn alongside their peers. It emphasizes
+									collaborative efforts among teachers, parents, and communities
+									to create supportive educational experiences for all students,
+									in line with international conventions and national laws.
+								</p>
+								<h2> Government-Funded Support for SPED </h2>
+								<p>
+									The Department of Education (DepEd) issued DepEd Order (DO)
+									No. 042, s. 2021 (Guidelines on the Utilization of Support
+									Funds for Special Education (SPED) Program) to provide
+									guidance on the rules for the release, allocation,
+									utilization, and liquidation/reporting of the SPED Program
+									Support Fund (PSF) for public elementary and secondary schools
+									offering the SPED Program, as well as regional offices and
+									schools division offices. The SPED PSF supports the
+									implementation of appropriate programs and services to
+									Learners with Disabilities.
+								</p>
 
-				<h2> Government Support and Policies Supporting SPED in the Philippines</h2>
-                <p>
-				The development of special education in the Philippines has received considerable support over the years from the government, 
-				non-government organizations, and stakeholders, addressing the needs and challenges of the times. 
-				Awareness among both government and private sectors regarding the importance of providing equal opportunities 
-				for children with special needs has significantly improved.
-                </p>
-				<br></br>
-                <h2> RA 10533 – The Enhanced Basic Education Act </h2>
-                <p>
-				Pursuant to Section 16 of Republic Act No. 10533, entitled "An Act Enhancing the Philippine Basic Education System by 
-				Strengthening Its Curriculum and Increasing the Number of Years for Basic Education, Appropriating Funds Therefor and for 
-				Other Purposes," otherwise known as the "Enhanced Basic Education Act of 2013," approved on May 15, 2013, 
-				and which took effect on June 8, 2013, the Department of Education (DepEd), the Commission on Higher Education (CHED), 
-				and the Technical Education and Skills Development Authority (TESDA), hereby issue the following rules and regulations 
-				to implement the provisions of the Act. 
-                </p>
-				<p>Republic Act No. 10533, also known as the "Enhanced Basic Education Act of 2013," introduced significant reforms 
-					to the Philippine education system. The Act aimed to strengthen the curriculum by aligning it with international 
-					standards and enhancing students' skillsets. It also increased the number of years in basic education through 
-					the implementation of the K-12 program, which includes Kindergarten, six years of elementary education, 
-					four years of junior high school, and two additional years of senior high school. Additionally, 
-					the Act ensured the allocation of funds to support improvements in infrastructure, teaching materials, 
-					and other resources needed to implement these reforms. The Department of Education (DepEd), 
-					the Commission on Higher Education (CHED), and the Technical Education and Skills Development Authority (TESDA) 
-					were tasked with executing the provisions of the law, focusing on different educational levels: 
-					DepEd for basic education, CHED for higher education, and TESDA for technical-vocational education. 
-					Ultimately, the law seeks to improve the overall quality of education in the Philippines, 
-					equipping students with the necessary skills for higher education and the workforce, 
-					and fostering global competitiveness.
-				</p>
-				<br></br>
-				<h2> RA 7277 – The Magna Carta for Disabled Persons</h2>
-                <p>
-				One positive development in special education is the implementation of Republic Act 7277, 
-				otherwise known as the Magna Carta for Disabled Persons, an Act providing for the rehabilitiation, 
-				self-development and self-reliance of disabled persons and their integration into the mainstream of society. 
-				Centers on employment, education, health care, social services, telecommunications, buildings and transportation, 
-				and political and civil rights. This Act also prohibits discrimination against disabled persons in employment, 
-				transportation, and in public accommodation and services. In support of this legislation, 
-				the Department of Education has directed all school divisions in the country to establish Special Education Centers 
-				to help provide effective delivery of special education services nationwide. 
-                </p>
-				<br></br>
-                <h2> DepEd Order No. 72, s. 2009 – Guidelines for SPED Programs</h2>
-                <p>
-				The Department of Education (DepEd) in the Philippines issued DepEd Order No. 72, s. 2009 
-				to promote inclusive education as a strategy to increase school participation among children, 
-				especially those with disabilities or special needs. The order encourages schools to adopt child-friendly 
-				environments and ensure that children with diverse learning needs are given the opportunity to learn alongside their peers.
-				It emphasizes collaborative efforts among teachers, parents, and communities to create supportive educational experiences for all students, 
-				in line with international conventions and national laws.
-                </p>
-				<h2> Government-Funded Support for SPED	</h2>
-                <p>
-				The Department of Education (DepEd) issued DepEd Order (DO) No. 042, s. 2021 (Guidelines on the Utilization of Support Funds for Special Education (SPED) Program) 
-				to provide guidance on the rules for the release, allocation, utilization, and liquidation/reporting of the SPED Program Support Fund (PSF) 
-				for public elementary and secondary schools offering the SPED Program, as well as regional offices and schools division offices. 
-				The SPED PSF supports the implementation of appropriate programs and services to Learners with Disabilities.
-                </p>
-
-                <div className="reference-section">
-						<br></br> <br></br> <br></br> {/* nilagyan ko lang ng gantong space para sa pic hehe */}
-						<br></br> <br></br> <br></br>
-						<br></br> <br></br> <br></br>
-                  <h3>References</h3>
-                  <p>[1] O. Seaman, "IRR of Republic Act No. 10533," Japan's Ministry of Education, Culture, Sports, Science and Technology, [Online]. Available: https://www.jpmac.or.jp/img/research/pdf_seamanlaw/O8_IRR-RA-10533-BSA.pdf. [Accessed: Oct. 23, 2024].</p>
-                  <p>[2] "Disability Inclusion in the Philippines," DisabilityIN, [Online]. Available: https://disabilityin.org/country/the-philippines/#:~:text=Legislation,into%20the%20mainstream%20of%20society. [Accessed: Oct. 23, 2024].</p>
-				  <p>[3] "DO 72, s. 2009: Inclusive Education as Strategy for Increasing Participation Rate of Children," Department of Education, Philippines, Jul. 6, 2009. [Online]. Available: https://www.deped.gov.ph/2009/07/06/do-72-s-2009-inclusive-education-as-strategy-for-increasing-participation-rate-of-children/. [Accessed: Oct. 23, 2024].</p>
-				  <p> </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
+								<div className="reference-section">
+									<br></br> <br></br> <br></br>{" "}
+									{/* nilagyan ko lang ng gantong space para sa pic hehe */}
+									<br></br> <br></br> <br></br>
+									<br></br> <br></br> <br></br>
+									<h3>References</h3>
+									<p>
+										[1] O. Seaman, "IRR of Republic Act No. 10533," Japan's
+										Ministry of Education, Culture, Sports, Science and
+										Technology, [Online]. Available:
+										https://www.jpmac.or.jp/img/research/pdf_seamanlaw/O8_IRR-RA-10533-BSA.pdf.
+										[Accessed: Oct. 23, 2024].
+									</p>
+									<p>
+										[2] "Disability Inclusion in the Philippines," DisabilityIN,
+										[Online]. Available:
+										https://disabilityin.org/country/the-philippines/#:~:text=Legislation,into%20the%20mainstream%20of%20society.
+										[Accessed: Oct. 23, 2024].
+									</p>
+									<p>
+										[3] "DO 72, s. 2009: Inclusive Education as Strategy for
+										Increasing Participation Rate of Children," Department of
+										Education, Philippines, Jul. 6, 2009. [Online]. Available:
+										https://www.deped.gov.ph/2009/07/06/do-72-s-2009-inclusive-education-as-strategy-for-increasing-participation-rate-of-children/.
+										[Accessed: Oct. 23, 2024].
+									</p>
+									<p> </p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default SupportSDES;
