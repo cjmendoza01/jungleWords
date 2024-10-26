47b2e2707d4edc02484ef25da2d1bfdab993be0b
cjmendoza01 <cyrill_mendoza@yahoo.com>
2024-10-26 19:41:59 +0800

    home

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/Route.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { isMobile } from "react-device-detect";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
@@ -15,13 +16,32 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				<meta http-equiv="X-Frame-Options" content="SAMEORIGIN" /> */}
 			{/* </Helmet> */}
 			<div>
-				<div className="desktop-only-message">
-					This app is only accessible on a desktop browser. Please switch to a
-					desktop device.
-				</div>
+				{isMobile && (
+					<div className="desktop-only-message">
+						This app is only accessible on a desktop browser. Please switch to a
+						desktop device.
+					</div>
+				)}
 
 				<div className="app-content">
-					<App />
+					{!isMobile ? (
+						<App />
+					) : (
+						<>
+							<div
+								style={{
+									width: "100vw",
+									height: "100vh",
+									display: "flex",
+									justifyContent: "center",
+									alignItems: "center",
+								}}
+							>
+								This app is only accessible on a desktop browser. Please switch
+								to a desktop device.{" "}
+							</div>
+						</>
+					)}
 				</div>
 			</div>
 		</BrowserRouter>
