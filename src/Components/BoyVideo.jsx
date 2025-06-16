401e4b55229aed92752c848dcf7fba01c8a28125
cyrill-knose <cyrill.mendoza@knose.com.au>
2025-06-16 13:57:01 +0800

    ch

--- src/Components/BoyVideo.jsx
+++ src/Components/BoyVideo.jsx
@@ -4,55 +4,55 @@ import "./BoyVideo.css"; // Import CSS for styling
 import button from "../assets/buttons&dialogues/skip.png";
 import muteButton from "../assets/buttons&dialogues/muteButton.png";
 import unmuteButton from "../assets/buttons&dialogues/unmuteButton.png";
-import boyVid from "/Boy.mp4";
+import boyVid from "/boy.mp4";
 
 const BoyVideo = () => {
-	const navigate = useNavigate();
-	const videoRef = useRef(null);
-	const [isMuted, setIsMuted] = useState(false); // Start unmuted
+  const navigate = useNavigate();
+  const videoRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(false); // Start unmuted
 
-	const handleVideoEnd = () => {
-		navigate("/LostBoy");
-	};
+  const handleVideoEnd = () => {
+    navigate("/LostBoy");
+  };
 
-	const handleSkip = () => {
-		navigate("/LostBoy");
-	};
+  const handleSkip = () => {
+    navigate("/LostBoy");
+  };
 
-	const toggleMute = () => {
-		setIsMuted(!isMuted);
-		if (videoRef.current) {
-			videoRef.current.muted = !isMuted;
-		}
-	};
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+    }
+  };
 
-	return (
-		<div className="BoyVideo">
-			<video
-				ref={videoRef}
-				autoPlay
-				muted={isMuted}
-				src={boyVid}
-				onEnded={handleVideoEnd}
-				className="video"
-			>
-				{/* <source  type="video/mp4" />
+  return (
+    <div className="BoyVideo">
+      <video
+        ref={videoRef}
+        autoPlay
+        muted={isMuted}
+        src={boyVid}
+        onEnded={handleVideoEnd}
+        className="video"
+      >
+        {/* <source  type="video/mp4" />
         Your browser does not support the video tag. */}
-			</video>
-			<img
-				src={button}
-				alt="Skip Button"
-				className="button skip-button"
-				onClick={handleSkip}
-			/>
-			<img
-				src={isMuted ? unmuteButton : muteButton}
-				alt="Mute/Unmute Button"
-				className="button mute-button"
-				onClick={toggleMute}
-			/>
-		</div>
-	);
+      </video>
+      <img
+        src={button}
+        alt="Skip Button"
+        className="button skip-button"
+        onClick={handleSkip}
+      />
+      <img
+        src={isMuted ? unmuteButton : muteButton}
+        alt="Mute/Unmute Button"
+        className="button mute-button"
+        onClick={toggleMute}
+      />
+    </div>
+  );
 };
 
 export default BoyVideo;
